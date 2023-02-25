322396e785d49ecd95759593df52c3f92f1b306a
alexiaaxels <112617700+alexiaaxels@users.noreply.github.com>
2023-02-25 13:38:41 +0000

    commit 3

--- sample.py
+++ sample.py
@@ -7,3 +7,39 @@ class Product():
         self.quantity = quantity
         self.tax = tax
 
+
+    def get_shipping(self):
+
+        #postcodes
+        city = range(300,400)
+        near_city = range(200,500)
+        outskirts = range(0,700)
+        if self.location in city: 
+            shipping = 0
+        elif self.location in near_city:
+            shipping = 500
+        elif self.location in outskirts:
+            shipping = 1000
+        else:
+            shipping = 2000 
+        
+        #weight in KG
+        small = range(0,1.5)
+        medium = range(1.5,3)
+        large = range(3,5)
+        if self.weight in small:
+            shipping *= 1.1
+        elif self.weight in medium:
+            shipping *= 1.2
+        elif self.weight in large:
+            shipping *= 1.3
+        else:
+            shipping *= 1.5
+        return shipping
+
+
+    def get_price(self):
+        shipping = self.get_shipping()
+        return (self.price + self.tax + shipping)*self.quantity
+    
+    
